fd3cc18e07d4d8a31bd0ee3b27795ee89fcdf604
PALLAB GANAI <pallabganai@PALLABs-MacBook-Pro.local>
2019-04-11 23:52:55 +1000

    JPA integrated

--- src/main/java/com/codenotfound/ws/endpoint/HelloWorldEndpoint.java
+++ src/main/java/com/codenotfound/ws/endpoint/HelloWorldEndpoint.java
@@ -2,10 +2,15 @@ package com.codenotfound.ws.endpoint;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.codenotfound.hello.Customer;
+import com.codenotfound.hello.CustomerRepository;
 import com.codenotfound.types.helloworld.Greeting;
 import com.codenotfound.types.helloworld.ObjectFactory;
 import com.codenotfound.types.helloworld.Person;
@@ -13,26 +18,64 @@ import com.codenotfound.types.helloworld.Person;
 @Endpoint
 public class HelloWorldEndpoint {
 
-  private static final Logger LOGGER =
-      LoggerFactory.getLogger(HelloWorldEndpoint.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldEndpoint.class);
 
-  @PayloadRoot(
-      namespace = "http://codenotfound.com/types/helloworld",
-      localPart = "person")
-  @ResponsePayload
-  public Greeting sayHello(@RequestPayload Person request) {
-    LOGGER.info("Endpoint received person[firstName={},lastName={}]",
-        request.getFirstName(), request.getLastName());
+	@Autowired
+	CustomerRepository customerRepository;
 
-    String greeting = "Hello " + request.getFirstName() + " "
-        + request.getLastName() + "!";
+	@PayloadRoot(namespace = "http://codenotfound.com/types/helloworld", localPart = "person")
+	@ResponsePayload
+	public Greeting sayHello(@RequestPayload Person request) {
+		LOGGER.info("Endpoint received person[firstName={},lastName={}]", request.getFirstName(),
+				request.getLastName());
 
-    ObjectFactory factory = new ObjectFactory();
-    Greeting response = factory.createGreeting();
-    response.setGreeting(greeting);
+		String greeting = "Hello " + request.getFirstName() + " " + request.getLastName() + "!";
 
-    LOGGER.info("Endpoint sending greeting='{}'",
-        response.getGreeting());
-    return response;
-  }
+		System.out.println("yahoooo");
+
+		this.demo();
+
+		ObjectFactory factory = new ObjectFactory();
+		Greeting response = factory.createGreeting();
+		response.setGreeting(greeting);
+
+		LOGGER.info("Endpoint sending greeting='{}'", response.getGreeting());
+		return response;
+	}
+
+	public void demo() {
+		// save a couple of customers
+		customerRepository.save(new Customer("Jack", "Bauer"));
+		customerRepository.save(new Customer("Chloe", "O'Brian"));
+		customerRepository.save(new Customer("Kim", "Bauer"));
+		customerRepository.save(new Customer("David", "Palmer"));
+		customerRepository.save(new Customer("Michelle", "Dessler"));
+
+		// fetch all customers
+		LOGGER.info("Customers found with findAll():");
+		LOGGER.info("-------------------------------");
+		for (Customer customer : customerRepository.findAll()) {
+			LOGGER.info(customer.toString());
+		}
+		LOGGER.info("");
+
+		// fetch an individual customer by ID
+		customerRepository.findById(1L).ifPresent(customer -> {
+			LOGGER.info("Customer found with findById(1L):");
+			LOGGER.info("--------------------------------");
+			LOGGER.info(customer.toString());
+			LOGGER.info("");
+		});
+
+		// fetch customers by last name
+		LOGGER.info("Customer found with findByLastName('Bauer'):");
+		LOGGER.info("--------------------------------------------");
+		customerRepository.findByLastName("Bauer").forEach(bauer -> {
+			LOGGER.info(bauer.toString());
+		});
+		// for (Customer bauer : repository.findByLastName("Bauer")) {
+		// LOGGER.info(bauer.toString());
+		// }
+		LOGGER.info("");
+	}
 }
